ecbfd410ab992318fc1378876b98ae049d267b30
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2020-10-30 23:37:32 +0300

    feat: добавить получение пользовательских данных через API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,37 @@
-import avatar from '../images/avatar.jpg';
+import React from 'react';
+
 import editAvatar from '../images/editAvatar.svg';
+import api from '../utils/api.js';
 
 function Main({onEditProfile, onAddPlace, onEditAvatar}) {
+
+  React.useEffect(() => {
+    api.getUserInfo().then((data) => {
+      setUserName(data.name);
+      setUserDescription(data.about);
+      setUserAvatar(data.avatar)
+    })
+  })
+
+  const [userName, setUserName] = React.useState();
+  const [userDescription, setUserDescription] = React.useState();
+  const [userAvatar, setUserAvatar] = React.useState();
+
 
   return (
     <main className="content">
       <section className="profile">
           <div className="profile__container">
             <div className="profile__avatar-wrapp">
-              <img src={avatar} alt="Аватар профиля" className="profile__avatar" />
+              <img src={`${userAvatar}`} alt="Аватар профиля" className="profile__avatar" />
               <img src={editAvatar} alt="Смена аватара" className="profile__avatar-edit" onClick={onEditAvatar}/>
             </div>
             <div className="profile__info">
               <div className="profile__wrap">
-                <h1 className="profile__name">Максим Хорс</h1>
+                <h1 className="profile__name">{userName}</h1>
                 <button type="button" className="profile__edit-button" onClick={onEditProfile}></button>
               </div>
-              <p className="profile__profession">Конный спортсмен</p>
+              <p className="profile__profession">{userDescription}</p>
             </div>
           </div>
           <button type="button" className="profile__add-button" onClick={onAddPlace}></button>
---
+++ src/utils/api.js
@@ -0,0 +1,127 @@
+class Api {
+  constructor({baseUrl, headers}){
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  // Получить начальные карточки
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {headers: this._headers})
+    .then(response => this._checkRequestResult(response))
+    .catch(error => this._errorHandler(error));
+  }
+
+  // Добавление новой карточки на сервер
+  addCard(name, link) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        link: link
+      })
+    })
+    .then(response => this._checkRequestResult(response))
+    .catch(error => this._errorHandler(error));
+  }
+
+  // Удалить карточку
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers,
+    })
+    .then(response => this._checkRequestResult(response))
+    .catch(error => this._errorHandler(error));
+  }
+
+  // Постановка лайка карточке
+  likeCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: 'PUT',
+      headers: this._headers,
+    })
+    .then(response => this._checkRequestResult(response))
+    .catch(error => this._errorHandler(error));
+  }
+
+  // Удаление лайка карточке
+  unlikeCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers,
+    })
+    .then(response => this._checkRequestResult(response))
+    .catch(error => this._errorHandler(error));
+  }
+  
+  // Получить данные пользователя
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {headers: this._headers})
+    .then(response => this._checkRequestResult(response))
+    .catch(error => this._errorHandler(error));
+  }
+
+  // Отредактировать данные пользователя
+  editUserInfo(name, profession) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        about: profession
+      })
+    })
+    .then(response => this._checkRequestResult(response))
+    .catch(error => this._errorHandler(error));
+  }
+
+  // Отредактировать аватар пользователя
+  editUserAvatar(urlAvatar) {
+    console.log(urlAvatar);
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: urlAvatar
+      })
+    })
+    .then(response => this._checkRequestResult(response))
+    .catch(error => this._errorHandler(error));
+  }
+
+  _checkRequestResult(response) {
+    if (response.ok) {
+      return response.json(); 
+    }
+    return Promise.reject(`Возникла ошибка: ${response.status}`); 
+  }
+
+  _errorHandler(error) {
+    console.log(error);
+  }
+}
+
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-16',
+  headers: {
+    authorization: 'e8e5e3a7-8ba0-46eb-8d27-e0f2fc68826d',
+    'Content-Type': 'application/json'
+  }
+}); 
+
+export default api;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
