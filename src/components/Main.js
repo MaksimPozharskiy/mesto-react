b579d6bf39911113d2f9b7134eb5822e34835b36
MaksimPozharskiy <Maksim.Pozharskiy@yandex.ru>
2020-11-14 15:22:33 +0300

    feat: добавить лайкайние карточек

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,11 +12,24 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
     })
   }, []);
 
-  /* {Карточки} */
+  // Карточки
   const [cards, setCards] = React.useState([]);
 
-  /* {Контекст} */
+  // Контекст
   const currentUser = React.useContext(CurrentUserContext);
+
+  function handleCardLike(card) {
+    // Проверяем, есть ли уже лайк на этой карточке
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    // Отправляем запрос в API и получаем обновлённые данные карточки
+    const changeLike = isLiked ? api.unlikeCard(card._id) : api.likeCard(card._id)
+    changeLike.then((newCard) => {
+    // Формируем новый массив на основе имеющегося, подставляя в него новую карточку
+    const newCards = cards.map((c) => c._id === card._id ? newCard : c);
+    // Обновляем стейт
+    setCards(newCards);
+  });
+  }
 
   return (
     <main className="content">
@@ -39,7 +52,12 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
     
       <section className="photos">
         <ul className="grid-photos">
-          {cards.map((card) => <Card  key={card._id} card={card} onCardClick={onCardClick} />)}
+          {cards.map((card) => <Card  
+            key={card._id} 
+            card={card} 
+            onCardClick={onCardClick}
+            onCardLike={handleCardLike}/>
+          )}
         </ul>
       </section>
     </main>
